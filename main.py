935dc72c21110cce58d554d7dbb0b22bc0d6c5fd
Half_nothing <Half_nothing@163.com>
2025-06-20 03:08:56 +0800

    fix: Fix no log file

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from PySide6.QtWidgets import QApplication, QStyleFactory
 
 from src.ui.main_window import MainWindow
 from src.utils import logger
+from src.utils.logger import logger_init
 
 
 def main() -> None:
+    logger_init()
     logger.info("Application started")
     app = QApplication(sys.argv)
     app.setStyle(QStyleFactory.create("light fusion"))
