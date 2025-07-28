78e23af572b133c8068568db46a0c0e304be7aec
Half_nothing <Half_nothing@163.com>
2025-07-29 04:52:14 +0800

    build: update build file

--- docs/build.bat
+++ docs/build.bat
@@ -1,1 +1,7 @@
-python -m nuitka --standalone --onefile --windows-console-mode=disable --enable-plugin=pyside6 --include-qt-plugins=platforms,styles --remove-output --follow-imports --nofollow-import-to=PySide6.QtWebEngine --nofollow-import-to=PySide6.Qt3D --nofollow-import-to=PySide6.QtBluetooth --nofollow-import-to=PySide6.QtSql --plugin-enable=pylint-warnings --output-dir=dist --windows-icon-from-ico=./icon/favicon.ico .\main.py
+@echo off
+set NUITKA_CACHE_DIR_CCACHE=D:\WorkSpace\python\SimpleCPDLC\build\cache\gcc
+set NUITKA_CACHE_DIR_CLCACHE=D:\WorkSpace\python\SimpleCPDLC\build\cache\msvc
+set NUITKA_CACHE_DIR_DOWNLOADS=D:\WorkSpace\python\SimpleCPDLC\build\cache\download
+set NUITKA_CACHE_DIR_BYTECODE=D:\WorkSpace\python\SimpleCPDLC\build\cache\byte
+set NUITKA_CACHE_DIR_DLL_DEPENDENCIES=D:\WorkSpace\python\SimpleCPDLC\build\cache\dll
+python -m nuitka --standalone --onefile --mingw64 --windows-console-mode=disable --enable-plugin=pyside6 --include-qt-plugins=platforms,styles --remove-output --follow-imports --nofollow-import-to=PySide6.QtWebEngine --nofollow-import-to=PySide6.Qt3D --nofollow-import-to=PySide6.QtBluetooth --nofollow-import-to=PySide6.QtSql --plugin-enable=pylint-warnings --output-dir=dist --windows-icon-from-ico=./icon/favicon.ico .\main.py
